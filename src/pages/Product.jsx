9137a9c00c4952510d882a43dbc78f708b3b5519
Kavishka Sandeepa <kavishkapk99@gmail.com>
2024-10-18 21:01:45 +0530

    product add form create successfully

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-
 function Product() {
+  const [products, setProduct] = useState([]);
+  const [category,setCategory]=useState([]);
 
-  const[products,setProduct]=useState([]);
+  const productNameRef=useRef(null);
+  const priceRef=useRef(0);
+  const quantityRef=useRef(0);
+  const categoryRef = useRef(0);
 
   useEffect(() => {
     fetchProduct();
+    fetchCategory();
   }, []);
 
   const fetchProduct = () => {
@@ -16,23 +21,106 @@ function Product() {
         return response.json();
       })
       .then((data) => {
-        setProduct(data)
+        setProduct(data);
         console.log(data);
       })
       .catch((e) => {
         console.log("error > ", e);
       });
   };
+
+  const fetchCategory = () => {
+    fetch(`http://localhost:8080/getAllCategory`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCategory(data);
+        console.log(data);
+      })
+      .catch((e) => {
+        console.log("error > ", e);
+      });
+  };
+
+  const addProduct = (data) => {
+    console.log(data);
+    fetch(`http://localhost:8080/addProduct`,{
+      method:'post',
+      headers:{'Content-Type': 'application/json'},
+      body:JSON.stringify(data)
+
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        fetchProduct()
+      })
+      .catch((e) => {
+        console.log("error > ", e);
+      });
+  };
+  const hadleSubmit=(e)=>{
+    e.preventDefault();
+    const data={
+      productName:productNameRef.current.value,
+      price:parseFloat(priceRef.current.value),
+      qty:parseFloat(quantityRef.current.value),
+      categoryId:parseInt(categoryRef.current.value)
+    }
+
+    addProduct(data);
+
+  }
+
   return (
     <div>
       <ul>
-        {products?.map((data,index)=>(
-            <li key={index}><Link to={`/product/${data.id}`}>{data.productName}</Link></li>
+        {products?.map((data) => (
+          <li key={data.id}>
+            <Link to={`/product/${data.id}`}>{data.productName}</Link>
+          </li>
         ))}
-        
       </ul>
-  </div>
-  )
+
+      <form onSubmit={hadleSubmit}>
+        <div>
+          <label htmlFor="productname">product name :</label>
+          <input id="productname" type="text"  ref={productNameRef}/>
+        </div>
+
+        <div>
+          <label htmlFor="">Price :</label>
+          <input type="number" ref={priceRef}/>
+        </div>
+
+        <div>
+          <label htmlFor="">Quantity :</label>
+          <input type="number" ref={quantityRef}/>
+        </div>
+        
+        <div>
+        <label htmlFor="">select your category :</label>
+          <select name="" id="dropdown" ref={categoryRef}>
+          <option value="">Select</option>
+           {
+              category.map((data)=>(
+              <option key={data.id} value={data.id}>{data.name}</option>
+          ))
+           }
+            
+          </select>
+          
+        </div>
+
+        <button type="submit" >add product</button>
+
+        
+      </form>
+    </div>
+  );
 }
 
 export default Product;
